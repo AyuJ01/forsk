fee1139841d601ed6624acb6fce59d356122c010
AyuJ01 <37456415+AyuJ01@users.noreply.github.com>
2018-06-22 20:18:29 +0530

    Update Ayushi_Jain_24.py

--- day7/Ayushi_Jain_24.py
+++ day7/Ayushi_Jain_24.py
@@ -7,5 +7,5 @@ Created on Fri May 18 14:18:23 2018
 
 
 import twitter
-api = twitter.Api(consumer_key="4An2yCQnINz57RC3e3vefArs5",consumer_secret="cjyQnI0vX0m6Wtm5noshNqPGY62dUcuwFYwDBVLuC9sgyVgC4O",access_token_key="3331576618-YI2x6JBO5JB1VWd10p8Pm7FhU4bDI6C10OMfyb7",access_token_secret="0HOU4UIe8LG8RjSPY8EKik123a7GwtifKAxJfzbXijsm6")
+api = twitter.Api(consumer_key="CONSUMER_KEY",consumer_secret="CONSUMER_SECRET",access_token_key="TOKEN_KEY",access_token_secret="TOKEN_SECRET")
 status = api.PostUpdate("Hello!!")
